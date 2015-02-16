fca4abe498ada2b7f194a663450ee8a7a637a793
stombor-tgm <sebastian.tombor@student.tgm.ac.at>
2015-02-16 13:57:31 +0100

    Hinzufügen eines Relationen Objektes für die einfachere Verarbeitung des ER Diagrammes

---
+++ Rueckwaertssalto/src/Relation.java
@@ -0,0 +1,69 @@
+
+public class Relation {
+	private String Notationtabelleone;
+	private String Notationtabelletwo;
+	private String special;
+	private Tabelle tabelleone;
+	private Tabelle tabelletwo;
+	/**
+	 * @return the notationtabelleone
+	 */
+	public String getNotationtabelleone() {
+		return Notationtabelleone;
+	}
+	/**
+	 * @param notationtabelleone the notationtabelleone to set
+	 */
+	public void setNotationtabelleone(String notationtabelleone) {
+		Notationtabelleone = notationtabelleone;
+	}
+	/**
+	 * @return the notationtabelletwo
+	 */
+	public String getNotationtabelletwo() {
+		return Notationtabelletwo;
+	}
+	/**
+	 * @param notationtabelletwo the notationtabelletwo to set
+	 */
+	public void setNotationtabelletwo(String notationtabelletwo) {
+		Notationtabelletwo = notationtabelletwo;
+	}
+	/**
+	 * @return the special
+	 */
+	public String getSpecial() {
+		return special;
+	}
+	/**
+	 * @param special the special to set
+	 */
+	public void setSpecial(String special) {
+		this.special = special;
+	}
+	/**
+	 * @return the tabelleone
+	 */
+	public Tabelle getTabelleone() {
+		return tabelleone;
+	}
+	/**
+	 * @param tabelleone the tabelleone to set
+	 */
+	public void setTabelleone(Tabelle tabelleone) {
+		this.tabelleone = tabelleone;
+	}
+	/**
+	 * @return the tabelletwo
+	 */
+	public Tabelle getTabelletwo() {
+		return tabelletwo;
+	}
+	/**
+	 * @param tabelletwo the tabelletwo to set
+	 */
+	public void setTabelletwo(Tabelle tabelletwo) {
+		this.tabelletwo = tabelletwo;
+	}
+
+}
